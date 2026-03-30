0e9447ed1441fa2951b35a34d8e671e0e122923c
Clinopyroxene <greenpyroxene@gmail.com>
2026-03-30 16:20:12 -0400

    Got enemy to spawn in random place and get larger with time to simulate distance. Started weapon swap

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
@@ -9,12 +11,16 @@ public class Enemy : MonoBehaviour
     public Vector2 bullet;
     public SpriteRenderer sr;
     public bool bye = false;
+    public Transform enemyTransform;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         eSpawnScript = GetComponentInParent<EnemySpawner>();
         sr = GetComponent<SpriteRenderer>();
+        enemyTransform = GetComponent<Transform>();
+        enemyTransform.localScale = Vector2.zero;
+        StartCoroutine(GetCloser());
     }
 
     // Update is called once per frame
@@ -24,6 +30,18 @@ public class Enemy : MonoBehaviour
         {
             bye = false;
             eSpawnScript.exist = false;
+        }
+    }
+
+    IEnumerator GetCloser()
+    {
+        float t = 0;
+
+        while(t < 2)
+        {
+            t += Time.deltaTime;
+            enemyTransform.localScale = new Vector2(0.25f, 0.25f) * t;
+            yield return null;
         }
     }
 }
--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -12,20 +12,57 @@ public class EnemySpawner : MonoBehaviour
     public GameObject enemyPrefab;
     public GameObject spawnedEnemy;
     public Vector2 bulletPos;
+    public Vector2 location;
     public bool check = false;
     public bool gone = false;
     public bool exist = false;
     public List<GameObject> enemies;
+    public float speed = 5.0f;
+    public float borderOne;
+    public float borderTwo;
+    public float borderThree;
+    public float borderFour;
+    public float divide;
+    public float divide2;
+    public float timer;
+    public float speedY;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        divide = (Screen.width / 6);
+        divide2 = (Screen.height / 6);
+        timer = 3;
     }
 
     // Update is called once per frame
     void Update()
-    { 
+    {
+        Vector2 newPos = transform.position;
+        newPos.x += speed * Time.deltaTime;
+        newPos.y += speedY * Time.deltaTime;
+        transform.position = newPos;
+        Vector2 screenPos = Camera.main.WorldToScreenPoint(transform.position);
+        borderOne = (Screen.width - Screen.width) + (divide);
+        borderTwo = Screen.width - (divide);
+        borderThree = (Screen.height - Screen.height) + (divide2);
+        borderFour = Screen.height - (divide2);
+        if (screenPos.x < borderOne || screenPos.x > borderTwo)
+        {
+            speed *= -1;
+        }
+        if (screenPos.y < borderThree || screenPos.y > borderFour)
+        {
+            speedY *= -1;
+        }
+        if(screenPos.x < borderOne - 30 || screenPos.x > borderTwo + 30)
+        {
+            transform.position = Vector2.zero;
+        }
+        if (screenPos.y < borderThree - 30 || screenPos.y > borderFour + 30)
+        {
+            transform.position = Vector2.zero;
+        }
         if (check == true)
         {
             bulletPos = spawnScript.GetBulletPos();
@@ -43,16 +80,20 @@ public class EnemySpawner : MonoBehaviour
                 spawnedEnemy = enemies[i];
             }
         }
-        if(Keyboard.current.spaceKey.wasPressedThisFrame)
+        timer -= Time.deltaTime;
+        if (timer <= 0)
         {
             exist = true;
-            spawnedEnemy = Instantiate(enemyPrefab, this.transform);
+            spawnedEnemy = Instantiate(enemyPrefab, transform.position, Quaternion.identity);
             enemies.Add(spawnedEnemy);
             for (int i = 0; i < enemies.Count; i++)
             {
                 spawnScript.srEnemy = enemies[i].GetComponent<SpriteRenderer>();
                 enemy = enemies[i].GetComponent<Enemy>();
             }
+            timer = 3;
+            speed = Random.Range(5, 10);
+            speedY = Random.Range(5, 10);
         }
     }
 }
